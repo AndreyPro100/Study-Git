0e1b5fee1b11bb88cfcbefe901294bea43f3ce9a
AndreyPro100 <andrey.kropotkin.00@gmail.com>
2022-03-23 16:59:31 +0300

    JS 191

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ let one = document.querySelector('#one')
 let two = document.querySelector('#two')
 let but = document.querySelector('#but')
 let elem = document.querySelector('#elem')
+let val = document.querySelector('#val')
+let val1 = document.querySelector('#val1')
 
+val.addEventListener('focus', function() {
+  val.value = ''
+  console.log(val.value)
+})
+
+val.addEventListener('blur', function() {
+  val.value = '2'
+  console.log((Number(val.value)) ** 2)
+})
 
 
 but.addEventListener('click', function () {
-  elem.width = 300;
+    /*  elem.width = 300;*/
+    /*  val.value += 'pass'*/
+  val1.value = Number(val.value)**2
 })
