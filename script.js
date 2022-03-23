7fc11d660017a14d96a68e70b07fb4d37cd466a7
AndreyPro100 <andrey.kropotkin.00@gmail.com>
2022-03-23 14:11:53 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let one = document.querySelector('#one')
+let two = document.querySelector('#two')
+let but = document.querySelector('#but')
+let elem = document.querySelector('#elem')
+
+
+
+but.addEventListener('click', function () {
+  elem.width = 300;
+})
